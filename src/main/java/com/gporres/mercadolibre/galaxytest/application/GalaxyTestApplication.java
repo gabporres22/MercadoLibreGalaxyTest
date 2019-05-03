96ca69b2be50b9b159213f284ea5a07c86f891eb
Gabriel Porres <gporres@tekgenesis.com>
2019-05-03 07:55:29 -0300

    First Commit

---
+++ src/main/java/com/gporres/mercadolibre/galaxytest/application/GalaxyTestApplication.java
@@ -0,0 +1,11 @@
+package com.gporres.mercadolibre.galaxytest.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxyTestApplication.class, args);
+    }
+}
